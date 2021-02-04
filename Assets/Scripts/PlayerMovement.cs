2ee3fb4d355ce2ed5b6f76de2a9a681e75735f69
Azathoth <m.sluijtman@sintlucasedu.nl>
2021-02-04 14:51:16 +0100

    Added first person controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    // Declaration of variables
+    private float x;
+    private float z;
+    private CharacterController controller;
+    private Vector3 move;
+    private float speed = 10f;
+    private Vector3 velocity;
+    private float gravity = -20f;
+    private float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    private void Start(){
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update(){
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
